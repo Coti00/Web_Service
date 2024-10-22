45e7b4250ac5702f2338e0d9906b5ee6e0a89842
김동준 <coticoger@Dongjun-MacBook.local>
2024-10-23 00:04:15 +0900

    test

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,14 @@
+import React,{useEffect} from "react";
 import styled from "styled-components";
 import Menu from "../components/Menu";
 import Ranking from "../components/Ranking";
 
 const MainPage = () => {
+
     return(
         <>
             <Menu/>
+            <div id="map" style={{width:'500px' ,height:'400px'}}></div>
             <Ranking></Ranking>
         </>
     );
